0ccd3fae14ad58089097fda301411adec96bc884
networkdowntime <networkdowntime@gmail.com>
2015-12-09 12:53:22 -0600

    New TextScrubber interface

--- src/main/java/net/networkdowntime/search/textProcessing/TextScrubber.java
+++ src/main/java/net/networkdowntime/search/textProcessing/TextScrubber.java
@@ -1,26 +1,7 @@
 package net.networkdowntime.search.textProcessing;
 
-/**
- * The TextScrubber is a preprocessor that does scrubbing of multi-word input text prior to splitting the content and scrubbing the keywords.
- * Removing HTML tags from the input.  HTML tags can contain additional fields that would lose context after splitting:
- * 	i.e. <input type="text"><input/>
- *  
- * @author rwiles
- *
- */
-public class TextScrubber {
+public interface TextScrubber {
 
-	private String htmlTagRegex = "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[^'\">\\s]+))?)+\\s*|\\s*)/?>";
+	public abstract String scrubText(String textToScrub);
 
-	public TextScrubber() {
-
-	}
-	
-	public String scrubText(String textToScrub) {
-		String retval;
-		
-		retval = textToScrub.replaceAll(htmlTagRegex, " ");
-		
-		return retval;
-	}
 }
