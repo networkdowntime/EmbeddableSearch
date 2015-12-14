abdf848fcfd00d534235ec5056232fd1c0aa89a9
networkdowntime <networkdowntime@gmail.com>
2015-12-13 22:55:44 -0600

    Updated README Added separate unit test for the Autocomplete class

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 Embeddable Search With Partial Word Auto-Complete
 ====
 
-A lightweight embeddable search engine for Java to provides robust auto-completion.  
+A lightweight embeddable search engine for Java that provides robust auto-completion.  Auto-completion can also be used stand-alone.  
 
-I've frequently lamented the ability to simply add search functionality to some of my java projects.  This search implementation strives to provide a lightweight in-memory search implementation that supports auto-completion and ordered search results.  
+I've frequently lamented the ability to simply add search functionality to some of my java projects.  This search implementation strives to provide a lightweight 
+in-memory search implementation that supports auto-completion and ordered search results.  
 
-At the moment the project provides the ability to store a long value as a search result for a given string.  This matches well with the typical pattern of storing a surrogate-key for the ID in the database allowing data to be indexed on application startup and the search results to be matched back to the data in the database.
+At the moment the project provides the ability to store a long value as a search result for a given string.  This matches well with the typical pattern of storing a surrogate-key for the ID in the database allowing data to be indexed on application startup and the search results to be matched back to the data in the database.  
+The search engine uses the auto-completion internally.
 
+Usage:
+----
+
+Stand-alone Autocomplete Usage:
+	Autocomplete autocomplete = new Autocomplete();
+	autocomplete.add("The quick brown fox jumps over the lazy dog");
+	autocomplete.getCompletions("uic browns", true, 10); // will contain "quick brown"
+	
+Search Usage:
+	SearchEngine searchEngine = new InMemorySearchEngine();
+	searchEngine.add(null, 6l, "The quick brown fox jumps over the lazy dog");
+	searchEngine.getCompletions("brown uic", false, 10); // will contain "quick brown"
+	Set<Long> results = searchEngine.search("uic browns", 3); // will contain 6
+
+	
 Implementation:
 ----
 Search functionality provided through a custom histogram implementation that tracks the different search results for a keyword and their counts.  Search results are
--- src/test/java/net/networkdowntime/search/engine/InMemorySearchEngineTest.java
+++ src/test/java/net/networkdowntime/search/engine/InMemorySearchEngineTest.java
@@ -10,43 +10,43 @@ import org.junit.Test;
 
 public class InMemorySearchEngineTest {
 
-	SearchEngine se = new InMemorySearchEngine();
+	SearchEngine searchEngine = new InMemorySearchEngine();
 
 	@Before
 	public void setUp() throws Exception {
-		se.add(null, 2l, "cacao");
-		se.add(null, 2l, "cacao");
-		se.add(null, 2l, "cacao");
-		se.add(null, 2l, "cacao");
+		searchEngine.add(null, 2l, "cacao");
+		searchEngine.add(null, 2l, "cacao");
+		searchEngine.add(null, 2l, "cacao");
+		searchEngine.add(null, 2l, "cacao");
 		
-		se.add(null, 5l, "ban");
+		searchEngine.add(null, 5l, "ban");
 
-		se.add(null, 4l, "bad");
-		se.add(null, 4l, "bad");
+		searchEngine.add(null, 4l, "bad");
+		searchEngine.add(null, 4l, "bad");
 
-		se.add(null, 0l, "band");
-		se.add(null, 0l, "band");
-		se.add(null, 0l, "band");
-		se.add(null, 0l, "band");
-		se.add(null, 0l, "band");
-		se.add(null, 0l, "band");
+		searchEngine.add(null, 0l, "band");
+		searchEngine.add(null, 0l, "band");
+		searchEngine.add(null, 0l, "band");
+		searchEngine.add(null, 0l, "band");
+		searchEngine.add(null, 0l, "band");
+		searchEngine.add(null, 0l, "band");
 
-		se.add(null, 1l, "banana");
-		se.add(null, 1l, "banana");
-		se.add(null, 1l, "banana");
-		se.add(null, 1l, "banana");
-		se.add(null, 1l, "banana");
+		searchEngine.add(null, 1l, "banana");
+		searchEngine.add(null, 1l, "banana");
+		searchEngine.add(null, 1l, "banana");
+		searchEngine.add(null, 1l, "banana");
+		searchEngine.add(null, 1l, "banana");
 
-		se.add(null, 3l, "bandy");
-		se.add(null, 3l, "bandy");
-		se.add(null, 3l, "bandy");
+		searchEngine.add(null, 3l, "bandy");
+		searchEngine.add(null, 3l, "bandy");
+		searchEngine.add(null, 3l, "bandy");
 		
-		se.add(null, 6l, "The quick brown fox jumps over the lazy dog");
+		searchEngine.add(null, 6l, "The quick brown fox jumps over the lazy dog");
 	}
 
 	@Test
 	public void testUnigramHistogramOrderedCommpletions() {
-		List<String> orderedList = se.getCompletions("a", false, 10);
+		List<String> orderedList = searchEngine.getCompletions("a", false, 10);
 
 		assertEquals("band", orderedList.get(0));
 		assertEquals("banana", orderedList.get(1));
@@ -59,7 +59,7 @@ public class InMemorySearchEngineTest {
 	
 	@Test
 	public void testSearchResults() {
-		List<String> orderedList = se.getCompletions("a", false, 10);
+		List<String> orderedList = searchEngine.getCompletions("a", false, 10);
 		assertEquals("band", orderedList.get(0));
 		assertEquals("banana", orderedList.get(1));
 		assertEquals("cacao", orderedList.get(2));
@@ -71,39 +71,39 @@ public class InMemorySearchEngineTest {
 	
 	@Test
 	public void testDigramHistogramCompletionFirstWordExcactMatch() {
-		List<String> orderedList = se.getCompletions("quick b", false, 10);
+		List<String> orderedList = searchEngine.getCompletions("quick b", false, 10);
 		assertEquals("quick brown", orderedList.get(0));
 	}
 
 	@Test
 	public void testDigramHistogramCompletionFirstWordFuzzyMatch() {
-		List<String> orderedList = se.getCompletions("uic b", true, 10);
+		List<String> orderedList = searchEngine.getCompletions("uic b", true, 10);
 		assertEquals("quick brown", orderedList.get(0));
 	}
 
 	@Test
 	public void testDigramHistogramCompletionFirstWordFuzzyMatch2() {
-		List<String> orderedList = se.getCompletions("uic browns", true, 10);
+		List<String> orderedList = searchEngine.getCompletions("uic browns", true, 10);
 		assertEquals("quick brown", orderedList.get(0));
 	}
 
 	@Test
 	public void testDigramHistogramCompletionFirstWordFuzzyMatch3() {
-		List<String> orderedList = se.getCompletions("uic bro", true, 10);
+		List<String> orderedList = searchEngine.getCompletions("uic bro", true, 10);
 		assertEquals(1, orderedList.size());
 		assertEquals("quick brown", orderedList.get(0));
 	}
 
 	@Test
 	public void testDigramHistogramCompletionWordReversal() {
-		List<String> orderedList = se.getCompletions("brown uic", false, 10);
+		List<String> orderedList = searchEngine.getCompletions("brown uic", false, 10);
 		assertEquals("quick brown", orderedList.get(0));
 	}
 
 	@Test
 	public void testSearchSingleElementMatch() {
-		se.add(null, 1l, "singleElement");
-		Set<Long> results = se.search("singleElement", 1);
+		searchEngine.add(null, 1l, "singleElement");
+		Set<Long> results = searchEngine.search("singleElement", 1);
 		assertEquals(1, results.size());
 		for (long result : results) {
 			assertEquals(1, result);
@@ -112,9 +112,9 @@ public class InMemorySearchEngineTest {
 
 	@Test
 	public void testSearchMultiResultMatch1() {
-		se.add(null, 1l, "multiResult1");
-		se.add(null, 1l, "multiResult1");
-		Set<Long> results = se.search("multiResult1", 1);
+		searchEngine.add(null, 1l, "multiResult1");
+		searchEngine.add(null, 1l, "multiResult1");
+		Set<Long> results = searchEngine.search("multiResult1", 1);
 		assertEquals(1, results.size());
 		for (long result : results) {
 			assertEquals(1, result);
@@ -123,13 +123,13 @@ public class InMemorySearchEngineTest {
 
 	@Test
 	public void testSearchMultiResultMatchOrdering() {
-		se.add(null, 1l, "multiResultMatchOrdering1");
-		se.add(null, 1l, "multiResultMatchOrdering1");
-		se.add(null, 2l, "multiResultMatchOrdering1");
-		se.add(null, 2l, "multiResultMatchOrdering1");
-		se.add(null, 2l, "multiResultMatchOrdering1");
-		se.add(null, 3l, "multiResultMatchOrdering1");
-		Set<Long> results = se.search("multiResultMatchOrdering1", 3);
+		searchEngine.add(null, 1l, "multiResultMatchOrdering1");
+		searchEngine.add(null, 1l, "multiResultMatchOrdering1");
+		searchEngine.add(null, 2l, "multiResultMatchOrdering1");
+		searchEngine.add(null, 2l, "multiResultMatchOrdering1");
+		searchEngine.add(null, 2l, "multiResultMatchOrdering1");
+		searchEngine.add(null, 3l, "multiResultMatchOrdering1");
+		Set<Long> results = searchEngine.search("multiResultMatchOrdering1", 3);
 		assertEquals(3, results.size());
 		
 		Long[] arr = results.toArray(new Long[0]);
@@ -147,7 +147,7 @@ public class InMemorySearchEngineTest {
 		long tenPercent = Math.round(numOfElementsToTest / 10.0d);
 		System.out.print("Test InMemorySearchEngine Capacity, Filling: ");
 		for (long i=0; i < numOfElementsToTest; i++) { 
-			se.add(null, i, (baseNumber + i) + "");
+			searchEngine.add(null, i, (baseNumber + i) + "");
 			if (i % tenPercent == 0) {
 				int percentage = (int) (100 * ((double) i / numOfElementsToTest));
 				if (percentage != 0)
@@ -159,7 +159,7 @@ public class InMemorySearchEngineTest {
 		System.out.print("\rSearching for test strings: ");
 		for (long i= 0; i < numOfElementsToTest; i++) { 
 			boolean foundResult = false;
-			for (Long result : se.search((baseNumber + i) + "", 10)) {
+			for (Long result : searchEngine.search((baseNumber + i) + "", 10)) {
 //				System.out.println(result);
 				foundResult |= result == i;
 			}
